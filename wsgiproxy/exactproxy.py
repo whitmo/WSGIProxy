57691043cdd0a02fd57d127181d67a2737dbc6f5
ianb <none@none>
2007-04-17 21:14:51 +0000

    Port fix from paste.proxy
    --HG--
    extra : convert_revision : svn%3A71705e6d-80f1-0310-9d5e-e4e1c580a33d/Paste/WSGIProxy/trunk%406473

--- wsgiproxy/exactproxy.py
+++ wsgiproxy/exactproxy.py
@@ -90,11 +90,7 @@ def proxy_exact_request(environ, start_response):
             return exc(environ, start_response)
         raise
     res = conn.getresponse()
-    headers_out = []
-    for full_header in res.msg.headers:
-        header, value = full_header.split(':', 1)
-        if header.lower() not in filtered_headers:
-            headers_out.append((header, value.strip()))
+    headers_out = parse_headers(res.msg)
     status = '%s %s' % (res.status, res.reason)
     start_response(status, headers_out)
     length = res.getheader('content-length')
@@ -105,3 +101,30 @@ def proxy_exact_request(environ, start_response):
         body = res.read()
     conn.close()
     return [body]
+
+def parse_headers(message):
+    """
+    Turn a Message object into a list of WSGI-style headers.
+    """
+    headers_out = []        
+    for full_header in message.headers:
+        if not full_header:            
+            # Shouldn't happen, but we'll just ignore
+            continue                     
+        if full_header[0].isspace():
+            # Continuation line, add to the last header
+            if not headers_out:                        
+                raise ValueError(
+                    "First header starts with a space (%r)" % full_header)
+            last_header, last_value = headers_out.pop()                   
+            value = last_value + ' ' + full_header.strip()
+            headers_out.append((last_header, value))      
+            continue                                
+        try:        
+            header, value = full_header.split(':', 1)
+        except:                                      
+            raise ValueError("Invalid header: %r" % full_header)
+        value = value.strip()                                   
+        if header.lower() not in filtered_headers:
+            headers_out.append((header, value))   
+    return headers_out
