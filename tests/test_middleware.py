0b626353eef1c4f09041a74ff305c0dbd8fdb75e
Marc Abramowitz <marca@surveymonkey.com>
2013-03-20 07:51:04 -0700

    tests/test_middleware.py: Make start_response take optional exc_info argument. Improve and uncomment test_exception_HTTPBadRequest.

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -3,7 +3,7 @@ import unittest
 from wsgiproxy.sampleapp import application
 from wsgiproxy.middleware import WSGIProxyMiddleware
 
-def start_response(status, headers):
+def start_response(status, headers, exc_info=None):
     # print('*** start_response: status=%r; headers=%r' % (status, headers))
     pass
 
@@ -16,13 +16,11 @@ class WSGIProxyMiddlewareTests(unittest.TestCase):
         environ = {}
         app(environ, start_response)
 
-    # This test triggers an error:
-    #
-    #   File "/Users/marca/dev/git-repos/WSGIProxy/wsgiproxy/middleware.py", line 162, in _fixup_environ
-    #     return exc(environ, start_response)
-    # NameError: global name 'start_response' is not defined
-    #
-    # def test_exception_HTTPBadRequest(self):
-    #     app = get_app()
-    #     environ = {'HTTP_X_SCRIPT_NAME': 'no_leading_slash_should_raise_HTTPBadRequest'}
-    #     app(environ, start_response)
+    def test_exception_HTTPBadRequest(self):
+        app = get_app()
+        environ = {
+            'HTTP_X_SCRIPT_NAME': 'no_leading_slash_should_raise_HTTPBadRequest',
+            'PATH_INFO': '/path/to/page',
+            'SCRIPT_NAME': 'foo.py',
+            }
+        app(environ, start_response)
