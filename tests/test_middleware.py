d083e6cdca7da46fe124c50463027762e147bfa0
Marc Abramowitz <marca@surveymonkey.com>
2013-03-20 09:29:39 -0700

    tests/test_middleware.py: Add more tests; increase coverage

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -62,3 +62,61 @@ class WSGIProxyMiddlewareTests(unittest.TestCase):
         self.assertEqual(result[6], "<tr><td>SCRIPT_NAME</td><td>''</td></tr>\n")
         self.assertEqual(result[7], "<tr><td>wsgi.url_scheme</td><td>'http'</td></tr>\n")
         self.assertEqual(result[8], '</table></body></html>')
+
+    def test_traversal_path_doesnt_start_with_slash(self):
+        app = get_app()
+        environ = {
+            'HTTP_X_TRAVERSAL_PATH': 'traversal/path',
+            'PATH_INFO': '/path/to/page',
+            'SCRIPT_NAME': 'foo.py',
+            }
+        result = app(environ, start_response)
+
+        # @todo: Should this return an error response?
+        self.assertEqual(len(result), 7)
+        self.assertEqual(result[0], '<html><head><title>Hello world</title></head>\n')
+        self.assertEqual(result[1], '<body><h1>Hello world!</h1>\n')
+        self.assertEqual(result[2], '<table border=1>\n')
+        self.assertEqual(result[3], "<tr><td>HTTP_X_TRAVERSAL_PATH</td><td>'traversal/path'</td></tr>\n")
+        self.assertEqual(result[4], "<tr><td>PATH_INFO</td><td>'/path/to/page'</td></tr>\n")
+        self.assertEqual(result[5], "<tr><td>SCRIPT_NAME</td><td>'foo.py'</td></tr>\n")
+        self.assertEqual(result[6], '</table></body></html>')
+
+    def test_traversal_path_equals_path_info(self):
+        app = get_app()
+        environ = {
+            'HTTP_X_TRAVERSAL_PATH': '/path/to/page',
+            'PATH_INFO': '/path/to/page',
+            'SCRIPT_NAME': 'foo.py',
+            }
+        result = app(environ, start_response)
+
+        # @todo: Should this return an error response?
+        self.assertEqual(len(result), 7)
+        self.assertEqual(result[0], '<html><head><title>Hello world</title></head>\n')
+        self.assertEqual(result[1], '<body><h1>Hello world!</h1>\n')
+        self.assertEqual(result[2], '<table border=1>\n')
+        self.assertEqual(result[3], "<tr><td>HTTP_X_TRAVERSAL_PATH</td><td>'/path/to/page'</td></tr>\n")
+        self.assertEqual(result[4], "<tr><td>PATH_INFO</td><td>''</td></tr>\n")
+        self.assertEqual(result[5], "<tr><td>SCRIPT_NAME</td><td>'foo.py'</td></tr>\n")
+        self.assertEqual(result[6], '</table></body></html>')
+
+    def test_traversal_path_subset_of_path_info(self):
+        app = get_app()
+        environ = {
+            'HTTP_X_TRAVERSAL_PATH': '/path/to/page',
+            'PATH_INFO': '/path/to/page/foo/bar',
+            'SCRIPT_NAME': 'foo.py',
+            }
+        result = app(environ, start_response)
+
+        # @todo: Should this return an error response?
+        self.assertEqual(len(result), 7)
+        self.assertEqual(result[0], '<html><head><title>Hello world</title></head>\n')
+        self.assertEqual(result[1], '<body><h1>Hello world!</h1>\n')
+        self.assertEqual(result[2], '<table border=1>\n')
+        self.assertEqual(result[3], "<tr><td>HTTP_X_TRAVERSAL_PATH</td><td>'/path/to/page'</td></tr>\n")
+        self.assertEqual(result[4], "<tr><td>PATH_INFO</td><td>'/foo/bar'</td></tr>\n")
+        self.assertEqual(result[5], "<tr><td>SCRIPT_NAME</td><td>'foo.py'</td></tr>\n")
+        self.assertEqual(result[6], '</table></body></html>')
+
